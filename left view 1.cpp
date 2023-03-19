62feda0deb4cd77544c5a21cf0fba7bd77f5c56a
chtarunkumar <78672369+chtarunkumar@users.noreply.github.com>
2023-03-19 17:08:16 +0530

    Add files via upload

---
+++ left view 1.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* newNode(int d)
+{
+    Node* t = new Node;
+    t->data = d;
+    t->left = NULL;
+    t->right = NULL;
+    return t;
+}
+
+void leftView(Node* node)
+{
+    if(node == NULL)
+        return;
+    queue<Node*> q;
+    q.push(node);
+    while(!q.empty())
+    {
+        int nodeCount = q.size();
+        for(int i=0; i<nodeCount; i++)
+        {
+            Node* t = q.front();
+            q.pop();
+            if(i == 0)
+                cout << t->data << " ";
+            if(t->left != NULL)
+                q.push(t->left);
+            if(t->right != NULL)
+                q.push(t->right);
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    Node* root = newNode(10);
+    root->left = newNode(20);
+    root->right = newNode(30);
+    root->left->left = newNode(40);
+    root->left->right = newNode(50);
+    root->right->left = newNode(60);
+    root->right->right = newNode(70);
+
+    leftView(root);
+}
+
+
+
